795bb3eda56c2767570863f2e62e117fb8607431
TonitoFleur <157834657+TonitoFleur@users.noreply.github.com>
2024-03-01 18:01:34 +0000

    Copie du main.py login et register

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask("test")
 
-
-@app.route('/')
+@app.route("/")
 def index():
-    return 'Hello from Flask!'
+  return render_template("index.html")
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=80)
+@app.route('/register')
+def register():
+  return render_template("register.html")
+
+@app.route('/contact')
+def contact():
+  return render_template("contact.html")
+
+@app.route('/sneakers')
+def sneakers():
+  return render_template("sneakers.html")
+
+@app.route('/phones')
+def phones():
+  return render_template("phones.html")
+
+@app.route('/clothes')
+def clothes():
+  return render_template("clothes.html")
+
+app.run(host="0.0.0.0", port=80)
